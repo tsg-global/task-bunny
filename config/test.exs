a106cfe0ab0bd9cdda72a5279b8c35550dba4536
Tin Hušnjak <tin.husnjak@gmail.com>
2024-02-14 21:01:43 +0100

    Add test config

--- config/test.exs
+++ config/test.exs
@@ -2,3 +2,11 @@ import Config
 
 config :logger, backends: [{LoggerFileBackend, :log_file}]
 config :logger, :log_file, level: :debug, path: "logs/test.log"
+
+config :task_bunny,
+  disable_auto_start: false,
+  hosts: [
+    default: [
+      connect_options: "amqp://rabbitmq:rabbitmq@localhost:5672/portal?heartbeat=30"
+    ]
+  ]
